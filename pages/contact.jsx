99bca341e63a4bf2bcbef302fa2a02d276090fdc
Lorenzo Andreotti <lorenzo.andreotti.2000@gmail.com>
2022-09-26 10:10:28 +0200

    Bugfix

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,10 +1,13 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { useForm } from "react-hook-form";
-
-let submitting = false;
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const Contact = () => {
+  const emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+  const [submitted, setSubmitted] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -20,132 +23,113 @@ const Contact = () => {
   };
 
   const onSubmit = (data) => {
-    console.log(data);
-    submitting = true;
-
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...data }),
     })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
-    submitting = false;
+      .then(() => console.log("Success!"))
+      .catch((error) => console.log(error));
+
+    setSubmitted(true);
+    console.log(data);
   };
-  //   console.log(errors);
 
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex flex-grow flex-col space-y-4 px-4">
         <h1 className="text-5xl font-bold text-green-500">Contact</h1>
-        <form
-          name="contact-form"
-          method="POST"
-          action="contact/?success=true"
-          data-netlify="true"
-          onSubmit={handleSubmit(onSubmit)}
-          className="mx-auto flex w-screen max-w-md flex-col space-y-4"
-        >
-          <input type="hidden" name="form-name" value="contact-form" />
-          <div className="flex flex-col space-y-2">
-            <label htmlFor="name">Name</label>
-            <input
-              className="border-green-300 focus:border-green-500 focus:outline-none focus:ring-green-500"
-              type="text"
-              name="name"
-              id="name"
-              {...register("name", { required: true, maxLength: 80 })}
-            />
-            <span className="text-red-500">
-              {errors.name?.type === "required" && "Name is required"}
-            </span>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <label htmlFor="email">Email</label>
-            <input
-              {...register("email")}
-              className="border-green-300 focus:border-green-500  focus:ring-green-500"
-              type="email"
-              name="email"
-              id="email"
-              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-            />
-            <span className="text-red-500">
-              {errors.email?.type === "required" && "email is required"}
-            </span>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <label htmlFor="select">Topic</label>
-            <select
-              {...register("topic", { required: true })}
-              className="border-green-300 focus:border-green-500  focus:ring-green-500"
-              name="select"
-              id="select"
-            >
-              <option value="1">Topic 1</option>
-              <option value="2">Topic 2</option>
-              <option value="3">Topic 3</option>
-            </select>
-            <span className="text-red-500">
-              {errors.topic?.type === "required" && "Topic is required"}
-            </span>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <label htmlFor="message">Message</label>
-            <textarea
-              {...register("message", { required: true })}
-              className="border-green-300 focus:border-green-500  focus:ring-green-500"
-              name="message"
-              id="message"
-              rows="5"
-            ></textarea>
-
-            <span className="text-red-500">
-              {errors.message?.type === "required" && "Message is required"}
-            </span>
-          </div>
-          <div className="flex flex-row items-center space-x-2">
-            <input
-              {...register("terms", { required: true })}
-              className="border-green-300 text-green-500  focus:border-green-500 focus:ring-green-500"
-              type="checkbox"
-              name="terms"
-              id="terms"
-            />
-            <label htmlFor="terms">I agree to the terms and conditions</label>
-
-            <span className="text-red-500">
-              {errors.terms?.type === "required" && "Terms is required"}
-            </span>
-          </div>
-          <button
-            type="submit"
-            className="flex items-center justify-center bg-green-500 px-3 py-2 text-white  focus:border-green-500 focus:outline-none focus:ring-green-500 active:bg-green-600"
-            disabled={submitting}
+        {!submitted && (
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto flex w-screen max-w-md flex-col space-y-4"
           >
-            {submitting ? (
-              <svg
-                aria-hidden="true"
-                class="mr-2 h-8 w-8 animate-spin fill-white text-green-700"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="name">Name</label>
+              <input
+                className="border-green-300 focus:border-green-500 focus:outline-none focus:ring-green-500"
+                type="text"
+                id="name"
+                {...register("name", { required: true, maxLength: 80 })}
+              />
+              <span className="text-red-500">
+                {errors.name?.type === "required" && "Name is required"}
+              </span>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="email">Email</label>
+              <input
+                {...register("email")}
+                className="border-green-300 focus:border-green-500  focus:ring-green-500"
+                type="email"
+                id="email"
+                {...register("email", {
+                  required: true,
+                  pattern: emailRegex,
+                })}
+              />
+              <span className="text-red-500">
+                {errors.email?.type === "required" && "email is required"}
+              </span>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="select">Topic</label>
+              <select
+                {...register("topic", { required: true })}
+                className="border-green-300 focus:border-green-500  focus:ring-green-500"
+                id="select"
               >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-            ) : (
-              "Submit"
-            )}
-          </button>
-        </form>
+                <option value="1">Topic 1</option>
+                <option value="2">Topic 2</option>
+                <option value="3">Topic 3</option>
+              </select>
+              <span className="text-red-500">
+                {errors.topic?.type === "required" && "Topic is required"}
+              </span>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="message">Message</label>
+              <textarea
+                {...register("message", { required: true })}
+                className="border-green-300 focus:border-green-500  focus:ring-green-500"
+                id="message"
+                rows="5"
+              ></textarea>
+
+              <span className="text-red-500">
+                {errors.message?.type === "required" && "Message is required"}
+              </span>
+            </div>
+            <div className="flex flex-row items-center space-x-2">
+              <input
+                {...register("terms", { required: true })}
+                className="border-green-300 text-green-500  focus:border-green-500 focus:ring-green-500"
+                type="checkbox"
+                id="terms"
+              />
+              <label htmlFor="terms">I agree to the terms and conditions</label>
+
+              <span className="text-red-500">
+                {errors.terms?.type === "required" && "Terms is required"}
+              </span>
+            </div>
+            <button
+              type="submit"
+              className="flex items-center justify-center bg-green-500 px-3 py-2 text-white  focus:border-green-500 focus:outline-none focus:ring-green-500 active:bg-green-600"
+              disabled={submitting}
+            >
+              Submit
+            </button>
+          </form>
+        )}
+        {submitted && (
+          <h5>Thanks for submitting! We&apos;ll reach out ASAP!</h5>
+        )}
       </main>
       <Footer />
     </div>
